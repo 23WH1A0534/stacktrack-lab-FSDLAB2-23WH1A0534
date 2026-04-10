1becc8ca39b24c0ae8faf2e4c86099bbc3936657
23wh1a0528 <23wh1a0528@bvrithyderabad.edu.in>
2026-04-10 12:55:53 +0530

    [INIT] Initial Project Setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Home from './pages/Home';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div>
       <Home />
 
-      {/* TODO: Replace this placeholder with Routes */}
+      {/* TODO: Replace this placeholder with Routes */
+      <BrowserRouter>
+      <Routes>
+        <Route path = "api/tasks" element = {<TaskList/>}/>
+        <Route path = "api/add-tasks" element = {<AddTask/>}/>
+      </Routes>
+      </BrowserRouter>
+      }
     </div>
   );
 }
